ae3d9060333d976a85db7cc5f48af490c8bd9e45
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-18 17:06:05 +0100

    zmiana

--- SalonSamochodowy/SalonSamochodowy/Produkt/Tables/RodzajSilnika.sql
+++ SalonSamochodowy/SalonSamochodowy/Produkt/Tables/RodzajSilnika.sql
@@ -2,7 +2,10 @@
     [RodzajSilnikaId] INT           NOT NULL,
     [Nazwa]           NVARCHAR (12) NOT NULL,
     [Opis]            NVARCHAR (1)  NULL,
+    [SurrogateKey]    INT           IDENTITY (1, 1) NOT NULL,
     PRIMARY KEY CLUSTERED ([RodzajSilnikaId] ASC),
     UNIQUE NONCLUSTERED ([Nazwa] ASC)
 );
+
+
 

97e375a027b701937a529639c455645c99ec98ae
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-18 17:50:38 +0100

    zmiana

---
+++ SalonSamochodowy/SalonSamochodowy/Produkt/Tables/Cennik.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Produkt].[Cennik] (
+    [CennikId]          INT   NOT NULL,
+    [ModelId]           INT   NOT NULL,
+    [DataDodania]       DATE  NOT NULL,
+    [DataObowiazywania] DATE  NOT NULL,
+    [CenaNominalna]     MONEY NULL,
+    [SurrogateKey]      INT   IDENTITY (1, 1) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CennikId] ASC)
+);
+
---
+++ SalonSamochodowy/SalonSamochodowy/Produkt/Tables/CennikWyposzazenia.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Produkt].[CennikWyposzazenia] (
+    [CennikWypoId]      INT   NOT NULL,
+    [WyposzazenieId]    INT   NULL,
+    [ModelId]           INT   NOT NULL,
+    [DataDodania]       DATE  NOT NULL,
+    [DataObowiazywania] DATE  NOT NULL,
+    [CenaNominalna]     MONEY NULL,
+    [SurrogateKey]      INT   IDENTITY (1, 1) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CennikWypoId] ASC)
+);
+

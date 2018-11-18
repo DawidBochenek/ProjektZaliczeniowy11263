3dc72214e161fcb1c0e06aafc4a72aeeef1ab653
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-18 17:56:12 +0100

    hr

---
+++ SalonSamochodowy/SalonSamochodowy/Hr/Tables/Pracownik.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [Hr].[Pracownik] (
+    [PracownikId]      INT          NOT NULL,
+    [Imie]             NVARCHAR (1) NOT NULL,
+    [Nazwisko]         NVARCHAR (1) NOT NULL,
+    [DataUrodzenia]    DATE         NOT NULL,
+    [DataZatrudnienia] DATE         NOT NULL,
+    [PESEL]            SMALLINT     NOT NULL,
+    [Adres]            NVARCHAR (1) NOT NULL,
+    [Miasto]           NVARCHAR (1) NOT NULL,
+    [KodPocztowy]      NVARCHAR (6) NOT NULL,
+    [Kraj]             NVARCHAR (1) NOT NULL,
+    [Telefon]          NVARCHAR (1) NOT NULL,
+    [StanowiskoId]     INT          NULL,
+    [ZespolId]         INT          NULL,
+    [Przelozony]       INT          NULL,
+    [SurrogateKey]     INT          IDENTITY (1, 1) NOT NULL,
+    PRIMARY KEY CLUSTERED ([PracownikId] ASC),
+    UNIQUE NONCLUSTERED ([PESEL] ASC)
+);
+
---
+++ SalonSamochodowy/SalonSamochodowy/Hr/Tables/Stanowisko.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Hr].[Stanowisko] (
+    [StanowiskoId] INT          NOT NULL,
+    [Nazwa]        NVARCHAR (1) NOT NULL,
+    [SurrogateKey] INT          IDENTITY (1, 1) NOT NULL,
+    PRIMARY KEY CLUSTERED ([StanowiskoId] ASC),
+    UNIQUE NONCLUSTERED ([Nazwa] ASC)
+);
+

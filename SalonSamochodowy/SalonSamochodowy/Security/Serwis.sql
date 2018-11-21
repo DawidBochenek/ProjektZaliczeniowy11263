39a27fea1f2ebbcf306b676b6622013a72ad2ba2
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-21 21:15:55 +0100

    zmiana

---
+++ SalonSamochodowy/SalonSamochodowy/Security/Serwis.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [Serwis]
+    AUTHORIZATION [dbo];
+
---
+++ SalonSamochodowy/SalonSamochodowy/Serwis/Tables/Uslugi.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Serwis].[Uslugi] (
+    [UslugiId]    INT          NOT NULL,
+    [UslugiKey]   INT          IDENTITY (1, 1) NOT NULL,
+    [NazwaUslugi] NVARCHAR (1) NOT NULL,
+    PRIMARY KEY CLUSTERED ([UslugiId] ASC)
+);
+

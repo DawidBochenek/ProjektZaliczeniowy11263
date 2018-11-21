f0772757b8c83411f8fbcf2be94964382167b3e2
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-21 20:52:02 +0100

    zmiana Hr.Wyplatt

--- SalonSamochodowy/SalonSamochodowy/Hr/Tables/WynagrodzeniaPracwonika.sql
+++ SalonSamochodowy/SalonSamochodowy/Hr/Tables/WynagrodzeniaPracwonika.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [Hr].[WynagrodzeniaPracwonika] (
     [WynId]                INT   NOT NULL,
-    [MiesiacRokId]         INT   NOT NULL,
     [WynagrodzeniaKey]     INT   IDENTITY (1, 1) NOT NULL,
+    [MiesiacRokId]         INT   NOT NULL,
     [OkresOd]              DATE  NULL,
     [OkresDo]              DATE  NULL,
     [WartoscWynagrodzenia] MONEY NULL,
-    CONSTRAINT [WynagrodzeniaId] PRIMARY KEY CLUSTERED ([WynId] ASC, [MiesiacRokId] ASC)
+    PRIMARY KEY CLUSTERED ([WynId] ASC)
 );
+
+
 
---
+++ SalonSamochodowy/SalonSamochodowy/Hr/Tables/Wyplaty.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Hr].[Wyplaty] (
+    [PracownikId] INT NOT NULL,
+    [WyplatyKey]  INT IDENTITY (1, 1) NOT NULL,
+    [WynId]       INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([PracownikId] ASC),
+    FOREIGN KEY ([WynId]) REFERENCES [Hr].[WynagrodzeniaPracwonika] ([WynId])
+);
+

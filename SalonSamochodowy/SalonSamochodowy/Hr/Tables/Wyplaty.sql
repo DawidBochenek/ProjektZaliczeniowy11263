18c53aadc857dc90316da47c41a3d2fb75199de9
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-22 20:23:47 +0100

    Hr wyplaty modyfikacja

--- SalonSamochodowy/SalonSamochodowy/Hr/Tables/Wyplaty.sql
+++ SalonSamochodowy/SalonSamochodowy/Hr/Tables/Wyplaty.sql
@@ -1,11 +1,15 @@
 ﻿CREATE TABLE [Hr].[Wyplaty] (
-    [PracownikId] INT NOT NULL,
-    [WyplatyKey]  INT IDENTITY (1, 1) NOT NULL,
-    [WynId]       INT NOT NULL,
-    [PremieId]    INT NULL,
+    [PracownikId]  INT NOT NULL,
+    [MiesiacRokId] INT NOT NULL,
+    [WynId]        INT NOT NULL,
+    [WyplatyKey]   INT IDENTITY (1, 1) NOT NULL,
+    [PremieId]     INT NULL,
+    PRIMARY KEY CLUSTERED ([PracownikId] ASC, [MiesiacRokId] ASC),
     FOREIGN KEY ([PremieId]) REFERENCES [Hr].[Premie] ([PremieId]),
     FOREIGN KEY ([WynId]) REFERENCES [Hr].[WynagrodzeniaPracwonika] ([WynId])
 );
+
+
 
 
 

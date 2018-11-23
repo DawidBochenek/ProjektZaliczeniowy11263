6501088b636dbd5b9a7cc5b5de5ee1581f2e8960
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-23 14:45:28 +0100

    Zmiana 2x tabel small int na int oraz dl pola navchar

--- SalonSamochodowy/SalonSamochodowy/Produkt/Tables/Do.sql
+++ SalonSamochodowy/SalonSamochodowy/Produkt/Tables/Do.sql
@@ -2,10 +2,12 @@
     [DoId]        INT           NOT NULL,
     [DoKey]       INT           IDENTITY (1, 1) NOT NULL,
     [Seria]       NVARCHAR (12) NOT NULL,
-    [WydanyPrzez] NVARCHAR (1)  NOT NULL,
+    [WydanyPrzez] NVARCHAR (45) NOT NULL,
     [WaznyDo]     DATE          NULL,
     PRIMARY KEY CLUSTERED ([DoId] ASC)
 );
+
+
 
 
 
--- SalonSamochodowy/SalonSamochodowy/Produkt/Tables/Klient.sql
+++ SalonSamochodowy/SalonSamochodowy/Produkt/Tables/Klient.sql
@@ -5,12 +5,14 @@
     [KlientImieDrugie]   NVARCHAR (20) NOT NULL,
     [KlientNazwisko]     NVARCHAR (35) NOT NULL,
     [DataDodania]        DATE          NOT NULL,
-    [Telefon]            SMALLINT      NOT NULL,
+    [Telefon]            INT           NOT NULL,
     [EmailId]            NVARCHAR (45) NOT NULL,
     [DowodOsobistyId]    INT           NULL,
     PRIMARY KEY CLUSTERED ([KlientId] ASC),
     FOREIGN KEY ([DowodOsobistyId]) REFERENCES [Produkt].[Do] ([DoId]) ON DELETE SET NULL
 );
+
+
 
 
 

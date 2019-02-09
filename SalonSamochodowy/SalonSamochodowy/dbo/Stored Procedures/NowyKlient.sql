e0574ae0ff5738213a0770f84d169b4c2d54be4b
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-09 14:49:51 +0100

    Procedura NowyKlient

---
+++ SalonSamochodowy/SalonSamochodowy/dbo/Stored Procedures/NowyKlient.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE NowyKlient
+
+@KlientId int,
+@KlientKey int,
+@KlientImiePierwsze nvarchar (20),
+@KlientImieDrugie nvarchar (20),
+@KlientNazwisko nvarchar (35),
+@DataDodania date,
+@Telefon int,
+@EmailId nvarchar (45),
+@DowodOsobistyId int
+AS
+BEGIN
+
+INSERT INTO Produkt.Klient (KlientId,KlientKey,KlientImiePierwsze,KlientImieDrugie,KlientNazwisko,DataDodania,Telefon,EmailId,DowodOsobistyId)
+VALUES (@KlientId,@KlientKey,@KlientImiePierwsze,@KlientImieDrugie,@KlientNazwisko,@DataDodania,@Telefon,@EmailId,@DowodOsobistyId);
+END

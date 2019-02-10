74e346108356a06ee4e8aedad2c4bccd62d22b2f
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-10 11:21:35 +0100

    zmiana hr.przcownik - miasto fk / poprawa nazwy kolumny MiastoId

--- SalonSamochodowy/SalonSamochodowy/Hr/Tables/Pracownik.sql
+++ SalonSamochodowy/SalonSamochodowy/Hr/Tables/Pracownik.sql
@@ -7,7 +7,7 @@
     [DataZatrudnienia] DATE          NOT NULL,
     [PESEL]            INT           NOT NULL,
     [Adres]            NVARCHAR (25) NOT NULL,
-    [Miasto]           NVARCHAR (25) NOT NULL,
+    [Miasto]           INT           NOT NULL,
     [KodPocztowy]      NVARCHAR (6)  NOT NULL,
     [Kraj]             NVARCHAR (25) NOT NULL,
     [Telefon]          NVARCHAR (20) NOT NULL,
@@ -18,8 +18,11 @@
     FOREIGN KEY ([Przelozony]) REFERENCES [Hr].[Pracownik] ([PracownikId]),
     FOREIGN KEY ([StanowiskoId]) REFERENCES [Hr].[Stanowisko] ([StanowiskoId]) ON DELETE SET NULL,
     FOREIGN KEY ([ZespolId]) REFERENCES [Hr].[Zespol] ([ZespolId]) ON DELETE SET NULL,
+    CONSTRAINT [Miasto] FOREIGN KEY ([Miasto]) REFERENCES [Wspolne].[Miasto] ([MiastoId]),
     UNIQUE NONCLUSTERED ([PESEL] ASC)
 );
+
+
 
 
 
---
+++ SalonSamochodowy/SalonSamochodowy/Wspolne/Tables/Miasto.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Wspolne].[Miasto] (
+    [MiastoId]   INT           NOT NULL,
+    [MiastoKey]  INT           NOT NULL,
+    [NazwaMista] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([MiastoId] ASC),
+    UNIQUE NONCLUSTERED ([NazwaMista] ASC)
+);
+
+
+

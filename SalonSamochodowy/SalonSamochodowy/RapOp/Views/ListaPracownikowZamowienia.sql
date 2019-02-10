54bad90b041912880b081826e3e9c228eae1de77
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-10 17:29:33 +0100

    widok częć c 4

---
+++ SalonSamochodowy/SalonSamochodowy/RapOp/Views/ListaPracownikowZamowienia.sql
@@ -0,0 +1,6 @@
+﻿
+Create view rapOp.ListaPracownikowZamowienia
+AS
+SELECT hrp.PracownikId,hrp.Imie,hrp.Nazwisko,z.WartosSprzedazy,z.IloscZamowien FROM Hr.Pracownik as hrp
+RIGHT JOIN (SELECT zam.PracownikId, SUM(zam.CenaSprzedazy)AS WartosSprzedazy ,COUNT(zam.ZamownienieId) AS IloscZamowien FROM Produkt.Zamownienie as zam
+GROUP BY zam.PracownikId) as z on hrp.PracownikId=z.PracownikId

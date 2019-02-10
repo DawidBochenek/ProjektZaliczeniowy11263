4bd7135cb59530d58d3b0c8dd72fddaaf971ffcd
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-10 14:00:46 +0100

    szczegoly zamoeniewia

---
+++ SalonSamochodowy/SalonSamochodowy/Produkt/Tables/SzczegolyZamowienia.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Produkt].[SzczegolyZamowienia] (
+    [ZamowienieId]   INT NOT NULL,
+    [WyposzazenieId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([ZamowienieId] ASC, [WyposzazenieId] ASC),
+    FOREIGN KEY ([WyposzazenieId]) REFERENCES [Produkt].[Wyposzazenie] ([WyposzazenieId])
+);
+
--- SalonSamochodowy/SalonSamochodowy/Produkt/Tables/Wyposzazenie.sql
+++ SalonSamochodowy/SalonSamochodowy/Produkt/Tables/Wyposzazenie.sql
@@ -3,10 +3,13 @@
     [WyposzazenieKey]  INT            IDENTITY (1, 1) NOT NULL,
     [Nazwa]            NVARCHAR (100) NOT NULL,
     [TypWyposazeniaId] INT            DEFAULT ((1)) NULL,
+    [CENA]             MONEY          NULL,
     PRIMARY KEY CLUSTERED ([WyposzazenieId] ASC),
     FOREIGN KEY ([TypWyposazeniaId]) REFERENCES [Produkt].[TypWyposazenia] ([TypWyposazeniaId]),
     UNIQUE NONCLUSTERED ([Nazwa] ASC)
 );
+
+
 
 
 

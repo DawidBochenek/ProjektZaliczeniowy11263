654c77530c92f9c05cc3b106d5f21ea5adf14f7d
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-10 16:59:24 +0100

    Nie sprzedane modele

---
+++ SalonSamochodowy/SalonSamochodowy/RapOp/Views/ModeleNieSprzedane.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW rapOp.ModeleNieSprzedane 
+AS
+SELECT m.ModelId,m.Nazwa,m.Rocznik FROM Produkt.Model as m
+LEFT JOIN Produkt.Zamownienie as zam on m.ModelId=zam.ModelId 
+WHERE ZamownienieId is null

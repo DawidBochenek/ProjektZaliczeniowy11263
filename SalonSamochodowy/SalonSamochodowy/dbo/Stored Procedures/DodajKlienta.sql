9f69ac37bf02748f72571098f0010ba6ea2ea508
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-10 10:44:20 +0100

    3x procedury plus zmiana tabela zamowienie

---
+++ SalonSamochodowy/SalonSamochodowy/dbo/Stored Procedures/DodajKlienta.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE DodajKlienta
+@ImieKlienta nvarchar (15),
+@Nazwisko nvarchar (25),
+@AdresUlica nvarchar (25),
+@NrLokalu nvarchar (15),
+@Miasto int,
+@KodPocztowy nvarchar (20),
+@Email nvarchar (250),
+@Telefon nvarchar (20),
+@KategoriaID int,
+@OpiekunKlienta int
+
+AS
+BEGIN
+	INSERT Klienci
+VALUES (@ImieKlienta, @Nazwisko, @AdresUlica, @NrLokalu, @Miasto, @KodPocztowy, @Email, @Telefon, @KategoriaID, @OpiekunKlienta)
+END

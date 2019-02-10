c87e8804e64a32285b79d331c510ba04ff9158f0
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-10 19:13:10 +0100

    rapOp.Cennik

---
+++ SalonSamochodowy/SalonSamochodowy/RapOp/Views/Cennik.sql
@@ -0,0 +1,8 @@
+﻿
+CREATE VIEW rapOp.Cennik
+AS
+-- Skrócona informacja dot. obowiązjącego cennika usług 
+
+SELECT cu.UslugiId,cu.DataObowOd,cu.DataObowDo, tu.NazwaUslugi AS TypUslugi,su.NazwaUslugi,cu.Cena  FROM Serwis.CennikUslugi cu
+LEFT JOIN Serwis.Uslugi as su on  cu.UslugiId=su.UslugiId
+LEFT JOIN Serwis.TypUslugi as tu on su.TypId=tu.TypUslugiId

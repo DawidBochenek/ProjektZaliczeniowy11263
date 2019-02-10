261a9ee02f4a75b78b33834897123a4026afaafd
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-10 11:37:59 +0100

    Zmiana Hr.Stanowiso nazwa na 60znaków

--- SalonSamochodowy/SalonSamochodowy/Hr/Tables/Stanowisko.sql
+++ SalonSamochodowy/SalonSamochodowy/Hr/Tables/Stanowisko.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [Hr].[Stanowisko] (
     [StanowiskoId]  INT           NOT NULL,
     [StanowiskoKey] INT           IDENTITY (1, 1) NOT NULL,
-    [Nazwa]         NVARCHAR (20) NOT NULL,
+    [Nazwa]         NVARCHAR (60) NOT NULL,
     PRIMARY KEY CLUSTERED ([StanowiskoId] ASC),
     UNIQUE NONCLUSTERED ([Nazwa] ASC)
 );
+
+
 
 
 

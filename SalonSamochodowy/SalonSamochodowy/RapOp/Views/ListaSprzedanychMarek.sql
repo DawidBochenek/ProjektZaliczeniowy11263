caefbcd2c46be93aafc2b8ad6995be98f35bd9fc
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-10 16:47:13 +0100

    rapOp widok sumaryczna sprzedaż wg marek

---
+++ SalonSamochodowy/SalonSamochodowy/RapOp/Views/ListaSprzedanychMarek.sql
@@ -0,0 +1,8 @@
+﻿
+
+CREATE VIEW rapOp.ListaSprzedanychMarek AS
+
+SELECT mark.NazwaSkr AS Marka, SUM(CenaSprzedazy) as SumarycznaKwotaSprzedazy, COUNT(mark.NazwaSkr) AS IloscZamowien FROM Produkt.Zamownienie as zam
+LEFT JOIN Produkt.Model as m on zam.ModelId=m.ModelId
+LEFT JOIN Produkt.Marka as mark on m.MarkaId=mark.MarkaId
+GROUP BY mark.NazwaSkr
--- SalonSamochodowy/SalonSamochodowy/Security/RapOp.sql
+++ SalonSamochodowy/SalonSamochodowy/Security/RapOp.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [RapOp]
     AUTHORIZATION [dbo];
 
+
+

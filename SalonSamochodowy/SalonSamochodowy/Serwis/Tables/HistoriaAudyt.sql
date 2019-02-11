df772ca041898bd618b9ac32f667d597c5e1e47c
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-11 19:06:29 +0100

    zmiana

---
+++ SalonSamochodowy/SalonSamochodowy/Serwis/Tables/HistoriaAudyt.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Serwis].[HistoriaAudyt] (
+    [AudytId]            INT            IDENTITY (1, 1) NOT NULL,
+    [ZamowienieId]       INT            NULL,
+    [StatusId]           INT            NULL,
+    [PracownikId]        INT            NULL,
+    [DataZmiany]         DATE           NULL,
+    [AktualizowanePrzez] NVARCHAR (128) NULL,
+    [AktualizowanoDnia]  DATE           NULL,
+    PRIMARY KEY CLUSTERED ([AudytId] ASC)
+);
+
---
+++ SalonSamochodowy/SalonSamochodowy/Serwis/Tables/HistoriaZamowienia.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [Serwis].[HistoriaZamowienia] (
+    [ZamowienieId] INT  NOT NULL,
+    [StatusId]     INT  NOT NULL,
+    [PracownikId]  INT  NOT NULL,
+    [DataZmiany]   DATE NULL,
+    PRIMARY KEY CLUSTERED ([ZamowienieId] ASC, [StatusId] ASC, [PracownikId] ASC),
+    FOREIGN KEY ([PracownikId]) REFERENCES [Hr].[Pracownik] ([PracownikId]),
+    FOREIGN KEY ([StatusId]) REFERENCES [Serwis].[StatusyZamowienie] ([StatusyZamowienieId]),
+    FOREIGN KEY ([ZamowienieId]) REFERENCES [Serwis].[Zamowienie] ([ZamowienieId])
+);
+
+
+GO
+create trigger Serwis.TriggerHistoriaAudyt on Serwis.HistoriaZamowienia
+after update, insert
+as
+begin
+insert into Serwis.HistoriaAudyt
+(ZamowienieId,PracownikId,StatusId,DataZmiany,AktualizowanePrzez,AktualizowanoDnia)
+select i.ZamowienieId,i.PracownikId,i.StatusId,i.DataZmiany, SUSER_NAME(),GETDATE()
+from Serwis.HistoriaZamowienia as t
+inner join inserted i on t.ZamowienieId=i.ZamowienieId
+end

081cde4b76449c58e42f69d434c11ed9f3685940
Dawid Bochenek <dawid.bochenek@microsoft.wsei.edu.pl>
2019-02-11 18:01:51 +0100

    with (readuncommitted)

--- SalonSamochodowy/SalonSamochodowy/RapOp/Views/ListaPracownikowZamowienia.sql
+++ SalonSamochodowy/SalonSamochodowy/RapOp/Views/ListaPracownikowZamowienia.sql
@@ -1,6 +1,6 @@
-﻿
-Create view rapOp.ListaPracownikowZamowienia
+﻿Create view rapOp.ListaPracownikowZamowienia
 AS
-SELECT hrp.PracownikId,hrp.Imie,hrp.Nazwisko,z.WartosSprzedazy,z.IloscZamowien FROM Hr.Pracownik as hrp
+
+SELECT hrp.PracownikId,hrp.Imie,hrp.Nazwisko,z.WartosSprzedazy,z.IloscZamowien FROM Hr.Pracownik as hrp  with (readuncommitted)
 RIGHT JOIN (SELECT zam.PracownikId, SUM(zam.CenaSprzedazy)AS WartosSprzedazy ,COUNT(zam.ZamownienieId) AS IloscZamowien FROM Produkt.Zamownienie as zam
 GROUP BY zam.PracownikId) as z on hrp.PracownikId=z.PracownikId
